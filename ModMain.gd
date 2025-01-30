b3e813865f4e48653f7b9579b7c14c0d1d311969
WT <126285619+1-WT-1@users.noreply.github.com>
2025-01-30 22:59:08 +0200

    0.2.0
    Added config system, thanks @rwqfsfasxc100﻿  for providing example.
    EI→Y Renamer mod compatibility (can be turned off in config)
    Control of ship dealer spawns in config
    Minor changes to torch firing commands.

--- ModMain.gd
+++ ModMain.gd
@@ -10,16 +10,30 @@ var modPath:String = get_script().resource_path.get_base_dir() + "/"
 # Required var for the replaceScene() func to work
 var _savedObjects := []
 
+var modConfig = {}
+# Initializes the configuration variable. Used by loadSettings.
+
+
 # Initialize the mod
 # This function is executed before the majority of the game is loaded
 # Only the Tool and Debug AutoLoads are available
 # Script and scene replacements should be done here, before the originals are loaded
 func _init(modLoader = ModLoader):
 	l("Initializing")
-	#loadDLC() #probably don't need this one.
-	replaceScene("enceladus/Upgrades.tscn")
+	# Modify Settings.gd first so we can load config and DLC
+	installScriptExtension("Settings.gd")
+	loadSettings()
 	
-	installScriptExtension("CurrentGame.gd")
+	loadDLC() 
+	
+	modsInstalled()
+
+	replaceScene("enceladus/Upgrades.tscn") #adding EIME_M Specific Torch
+	
+	if modConfig["settings"]["addToUsedShipPool"]:
+	#and modDependancy.has("Derelict-Delights-1.4.1.zip"):
+		installScriptExtension("CurrentGame.gd")
+		l("Added EIME_M to used ship pool")
 	installScriptExtension("ships/Shipyard.gd")
 	l("Initialized")
 
@@ -27,10 +41,14 @@ func _init(modLoader = ModLoader):
 # At this point all AutoLoads are available and the game is loaded
 func _ready():
 	l("Readying")
-	replaceScene("ships/EIME_M.tscn")
-	updateTL("i18n/en.txt", "|")
-	updateTL("i18n/ua.txt", "|")
-
+	replaceScene("ships/EIME_M.tscn") #Had to load on ready because of inheritance
+	if modDependancy.has("Y.zip") and modConfig["settings"]["EIRename-Y"]:
+		updateTL("i18n/Yen.txt", "|")
+		updateTL("i18n/Yua.txt", "|")
+		l("EI Renamed to Y") 
+	else:
+		updateTL("i18n/en.txt", "|")
+		updateTL("i18n/ua.txt", "|")
 	l("Ready")
 	
 # Helper script to load translations using csv format
@@ -114,7 +132,46 @@ func loadDLC():
 # Func to print messages to the logs
 func l(msg:String, title:String = MOD_NAME):
 	Debug.l("[%s]: %s" % [title, msg])
+
+
+func loadSettings():
+	l(MOD_NAME + ": Loading mod settings")
+	var settings = load("res://Settings.gd").new()
+	settings.loadEIME_M_FromFile()
+	settings.saveEIME_M_ToFile()
+	modConfig = settings.EIME_M_Config
+	l(MOD_NAME + ": Current settings: %s" % modConfig)
+	settings.queue_free()
+	l(MOD_NAME + ": Finished loading settings")
 	
 	
-	
-	
+var modDependancy = []
+func modsInstalled():
+	var gameInstallDirectory = OS.get_executable_path().get_base_dir()
+	if OS.get_name() == "OSX":
+		gameInstallDirectory = gameInstallDirectory.get_base_dir().get_base_dir().get_base_dir()
+	var modPathPrefix = gameInstallDirectory.plus_file("mods")
+	l(MOD_NAME + ": Registering and verifying contents of the mods folder")
+	var dir = Directory.new()
+	dir.open(modPathPrefix)
+	var dirName = dir.get_current_dir()
+	dir.list_dir_begin(true)
+	while true:
+		var fileName = dir.get_next()
+		dirName = dir.get_current_dir()
+		l(fileName)
+		if fileName == "":
+			break
+		if dir.current_is_dir():
+			continue
+		var modFSPath = modPathPrefix.plus_file(fileName)
+		var modGlobalPath = ProjectSettings.globalize_path(modFSPath)
+		if not ProjectSettings.load_resource_pack(modGlobalPath, true):
+			l(MOD_NAME + ": %s failed to register." % fileName)
+			continue
+		var trueFileName = modFSPath.split("/")
+		var trueFileNameLength = trueFileName.size()
+		var getTrueFileName = trueFileName[trueFileNameLength - 1]
+		modDependancy.append(getTrueFileName)
+		l(MOD_NAME + ": %s registered." % fileName)
+	l(MOD_NAME + ": Finished verification of mod folder.")
---
+++ Settings.gd
@@ -0,0 +1,52 @@
+extends "res://Settings.gd"
+
+# Mod version
+const EIME_M_VERSION = "0.2.0"
+
+
+# You may want to change many of the variable names to provide a unique identifier
+# Make sure anything read by the ModMain is consistent with this file or they will not work
+# These are default config values
+# Any value not set in the config file will generate the missing values exactly as these are
+var EIME_M_Config = {
+	"settings":{
+		"EIRename-Y":true,
+		"addToUsedShipPool":true,
+	}, 
+}
+
+# The config file name. Make sure you set something unique
+var EIME_M_Path = "user://EIME_M_settings.cfg"
+var EIME_M_File = ConfigFile.new()
+
+func _ready():
+	loadEIME_M_FromFile()
+	saveEIME_M_ToFile()
+
+
+func saveEIME_M_ToFile():
+	for section in EIME_M_Config:
+		for key in EIME_M_Config[section]:
+			EIME_M_File.set_value(section, key, EIME_M_Config[section][key])
+	EIME_M_File.save(EIME_M_Path)
+
+
+func loadEIME_M_FromFile():
+	var error = EIME_M_File.load(EIME_M_Path)
+	if error != OK:
+		Debug.l("EIME_M_ Mod: Error loading settings %s" % error)
+		return 
+	for section in EIME_M_Config:
+		for key in EIME_M_Config[section]:
+			EIME_M_Config[section][key] = EIME_M_File.get_value(section, key, EIME_M_Config[section][key])
+	loadEIME_M_KeymapFromConfig()
+
+# Keybind setting handlers
+func loadEIME_M_KeymapFromConfig():
+	for action_name in EIME_M_Config.input:
+		InputMap.add_action(action_name)
+		for key in EIME_M_Config.input[action_name]:
+			var event = InputEventKey.new()
+			event.scancode = OS.find_scancode_from_string(key)
+			InputMap.action_add_event(action_name, event)
+	emit_signal("controlSchemeChaged")
